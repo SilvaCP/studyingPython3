6d1f497b3cdf7510ab377e6eb5ca9fc6d91e78f3
SilvaCP <silvaotl@outlockcom>
2025-05-30 22:43:03 -0300

    Copiei um código do ChatGpt

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
-print("Olá Mundo!")
-print("Ok deu certo!")
-print("Outra alteração mas desta vez usando a IDE de Pycharm!")
+'''
+Esse é um comentário que pode ser de diversas linhas.
+OK!
+OK! Estudos de:
+Operadores lógicos / e Condicionáis.
+'''
+# Primeiro exemplo.
+'''
+a = False #True
+b = False
+if a or b:
+    print("Atendeu a condição!")
+else:
+    print("Não atendeu!")
+'''
+'''
+# Segundo exemplo.
+nome = "Filipe"
+idade = 24
+peso = 90
+if idade == 25 and nome == "Filipe" and peso == 90: # !=25:
+    print("Idade correta!")
+else:
+    print("Idade incorreta!")
+'''
+import calendar
+
+# Solicita ao usuário o mês e o ano
+mes = int(input("Digite o mês de nascimento: "))
+ano = int(input("Digite o ano de nascimento: "))
+
+# Define o dia 1 do mês e ano fornecidos
+dia_da_semana = calendar.weekday(ano, mes, 1)
+
+# Lista com nomes dos dias da semana
+dias = ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo']
+
+# Exibe o resultado
+print(f"O dia da semana do seu nascimento foi: {dias[dia_da_semana]}")
